c4a60a7b8e2deb0afca63837a77516b35614d218
Muhammad Bilal <raobilalhameed@gmail.com>
2023-07-03 13:04:02 +0500

    notification config

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,10 +2,18 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCkcKaQjOoknwRBVAVU7B9RSuGy23ZyLXI",
-  authDomain: "maxxitech-f73cf.firebaseapp.com",
-  projectId: "maxxitech-f73cf",
-  storageBucket: "maxxitech-f73cf.appspot.com",
-  messagingSenderId: "23146168208",
-  appId: "1:23146168208:web:a29a65a9b61e8f31d1bd15",
+    apiKey: "AIzaSyCkcKaQjOoknwRBVAVU7B9RSuGy23ZyLXI",
+    authDomain: "maxxitech-f73cf.firebaseapp.com",
+    projectId: "maxxitech-f73cf",
+    storageBucket: "maxxitech-f73cf.appspot.com",
+    messagingSenderId: "23146168208",
+    appId: "1:23146168208:web:a29a65a9b61e8f31d1bd15",
+    measurementId: "G-H161EMPWZK"
+});
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
 });
